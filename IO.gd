a573a2c35ab5bfa369d3576814597470c107dfc6
Tommaso De Tommaso <detommaso23@gmail.com>
2022-12-18 16:09:59 +0100

    Updated Input Output module

--- IO.gd
+++ IO.gd
@@ -2,22 +2,23 @@ class_name IO
 
 static func get_scene_path(scene_name: String, game_name: String = "") -> String:
 	if(game_name != ""):
-		return "res://" + game_name + "/" + scene_name
+		return "res://" + game_name + "/" + scene_name + ".tscn"
 	else:
-		return "res://" + scene_name
+		return "res://" + scene_name + ".tscn"
 
 static func search_scene_path(scene_name: String) -> String:
-	var dirs_to_explore: Array
 	var current_dir : DirAccess = DirAccess.open("res://")
-	var scene_found : bool = false
-	while ! scene_found:
-		for file in current_dir.get_files():
-			if file == (scene_name + ".tscn"):
-				scene_found = true
-		dirs_to_explore.append_array(current_dir.get_directories())
-		current_dir.change_dir(dirs_to_explore.pop_back())
+	var scene_index : int = current_dir.get_files().find(scene_name)
+	var dirs_to_explore: Array
 	
-	if scene_found:
+	while scene_index == -1:
+		for dir in current_dir.get_directories():
+			dirs_to_explore.append(current_dir.get_current_dir() + "/" + dir)
+		if(dirs_to_explore.size() > 0):
+			current_dir.change_dir(dirs_to_explore.pop_back())
+		scene_index = current_dir.get_files().find(scene_name)
+	
+	if scene_index:
 		return current_dir.get_current_dir() + "/" + scene_name + ".tscn"
 	else:
 		return ""
